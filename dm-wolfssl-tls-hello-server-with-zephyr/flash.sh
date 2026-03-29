a5a5b40ec50ed9f7f641dc010d10956653e93293
Yosuke Shimizu <yosuke@wolfssl.com>
2026-03-30 08:52:55 +0900

    Add helper scripts

---
+++ dm-wolfssl-tls-hello-server-with-zephyr/flash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/Applications/LinkServer_25.12.83/LinkServer flash  MCXN947 load debug/zephyr/zephyr_stripped_v1_signed.bin:0xA000 debug/zephyr/wolfboot.bin:0x0
---
+++ dm-wolfssl-tls-hello-server-with-zephyr/strip.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dd if=debug/zephyr/zephyr.bin of=debug/zephyr/zephyr_stripped.bin bs=1 skip=512
